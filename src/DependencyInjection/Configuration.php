fbf407bbc8e8220cc305a00b26c1e36f920b555b
Oskar Gunther <o.gunther@consultia.pl>
2019-11-14 11:52:11 +0100

    config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,6 +18,8 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('search_bundle')->children()
+            ->end();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
